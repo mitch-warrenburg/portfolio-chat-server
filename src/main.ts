594bcbaadf7bf430026b0cfdc0db063bc7f873a5
Mitch Warrenburg <mitch_warrenburg@keybank.com>
2021-03-16 04:23:49 -0700

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,36 @@
+import morgan from 'morgan';
+import express from 'express';
 import { Server } from 'socket.io';
+import connect from 'connect-redis';
 import { createServer } from 'http';
+import { RedisClient } from 'redis';
+import session from 'express-session';
+import { createAdapter } from 'socket.io-redis';
 
 const PORT = 9000;
 const NEW_CHAT_MESSAGE_EVENT = 'newChatMessage';
 
-const httpServer = createServer();
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: '*',
-  },
-});
+const app = express();
+const RedisStore = connect(session);
+const redisClient = new RedisClient({ host: 'localhost', port: 6379 });
+const subClient = redisClient.duplicate();
+const pubClient = redisClient.duplicate();
+const httpServer = createServer();
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+io.use(wrap(session({ secret: 'cats' })));
+io.adapter(createAdapter({ pubClient, subClient }));
+io.adapter(createAdapter({ pubClient, subClient }));
 
 io.on('connection', (socket) => {
   // Join a conversation
   const roomId = socket.handshake.query.roomId as string;
   socket.join(roomId);
+
+  //@ts-ignore
+  console.log(socket.request.session);
 
   // Listen for new messages
   socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
@@ -27,6 +42,35 @@ io.on('connection', (socket) => {
     socket.leave(roomId);
   });
 });
+
+
+// @ts-ignore
+morgan.token('sessionid', (req) => req.sessionID);
+
+// @ts-ignore
+morgan.token('user', req => req.session.user);
+
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :user :sessionid'));
+
+app.get('/', (_, res) => {
+  res.send('Hitarth is a fat moron.');
+});
+
+app.use(
+  session({
+    secret: 'veryimportantsecret',
+    resave: false,
+    name: 'secret',
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      sameSite: true,
+      maxAge: 600000,
+    },
+    store: new RedisStore({ client: redisClient, ttl: 86400 }),
+  }),
+);
+
 
 httpServer.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
